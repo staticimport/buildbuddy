297fb4d0e8a31ca044f83ad9b7ece7d40c4ee679
Craig Bowles <staticimport@gmail.com>
2015-06-08 20:29:06 -0500

    changed compile flags

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-OPTIMIZE='-O0 -fno-strict-aliasing'
+OPTIMIZE='-O2 -fno-strict-aliasing'
 WARNS='-Wall -Wextra -Werror'
 
 set -e
 mkdir -p bin
 rm -f bin/*
-gcc -std=gnu99 -ggdb3 $OPTIMIZE $WARNS -Isrc src/main.c -o bin/buildbuddy
+gcc -std=gnu99 $OPTIMIZE $WARNS -Isrc src/main.c -o bin/buildbuddy
 
